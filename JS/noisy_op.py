b37a300dc43fc1a2ed6a25725641c5331832dc36
xzzjx <jxzheng95@gmail.com>
2018-11-12 09:20:22 +0800

    batch vote done

--- JS/noisy_op.py
+++ JS/noisy_op.py
@@ -26,9 +26,11 @@ def compute_loss(s, t, graph, name=None):
                                 graph,
                                 name=name,
                                 # grad=backprop_func_noise)
-                                grad=backprop_func_batch_noise)
+                                # grad=backprop_func_batch_noise)
                                 # grad=kl_backprop_func_noise)
                                 # grad=backprop_func_bt)
+                                # grad = vote_gradient)
+                                grad = batch_vote_gradient)
 
 def py_func(func, inp, Tout, graph, stateful=True, name=None, grad=None):
     
@@ -378,6 +380,192 @@ def backprop_func_bt(op, grad):
     d = tf.expand_dims(d, 1)
 
     s_grad = d*grad/FLAGS.batch_size
+
+    t_grad = tf.constant(np.zeros(10, dtype=np.float32))
+
+    t_grad = tf.tile(t_grad, [t.get_shape()[0]*t.get_shape()[1]])
+    t_grad = tf.reshape(t_grad, t.get_shape())
+
+    return s_grad, t_grad
+
+def bincount_(x):
+    return tf.bincount(x, minlength=12)
+def bincount(x):
+    # return tf.bincount(x, minlength=FLAGS.nb_labels)
+    return tf.map_fn(bincount_, x)
+# def bincount_pyfunc(x):
+#     result = np.zeros((x.shape[0], x.shape[1]))
+#     for i in range(x.shape[0]):
+#         for j in range(x.shape[1]):
+#             bins = np.bincount(x[i][j], minlength=11)
+#             result[i][j] = np.argmax(bins)
+#     return result
+def lap_noise_vote(shape):
+    delta_f = 1
+    epsilon = FLAGS.lap_epsilon
+    b = delta_f / epsilon
+    lap_dist = tf.distributions.Laplace(loc=0.0, scale=b)
+    lap_noise = lap_dist.sample(shape)
+    return lap_noise
+
+def vote_gradient(op, grad):
+    '''
+    将梯度划分成11段，分别是1, 0, -1, -2, -3...-10
+    每个teacher的梯度为最近的段点投票
+    加噪音后输出段点的平均值作为该样本的梯度
+    一个batch的梯度取平均值
+    '''
+    s = op.inputs[0] # shape=(128, 1, 10)
+    t = op.inputs[1] # shape=(128, 100, 10)
+
+    denominator = tf.add(s, t, name='denominator')
+    tmp = 2.0*s/(denominator+1e-14) + 1e-14
+    d_ = tf.log(tmp, name='derivation')
+    d_ = d_ / np.log(2.0)
+    # d_ = 0.5*d_   # shape=(128, 100, 10) # TODO
+
+    d_ = tf.clip_by_value(d_, clip_value_min=-10.0, clip_value_max=1.0)
+    # d_ = tf.round(d_) # shape=(128, 100, 10)
+    ones = tf.ones_like(d_, dtype=tf.float32)
+    # ones = tf.constant(np.ones([FLAGS.batch_size, FLAGS.nb_teachers, FLAGS.nb_labels]))
+    # zeros = tf.constant(np.zeros([FLAGS.batch_size, FLAGS.nb_teachers, FLAGS.nb_labels]))    
+    zeros = tf.zeros_like(d_, dtype=tf.float32)
+    twos = ones * 2
+    # val_list = [0.0, 1.0, -10.0]
+    d_1 = tf.where(tf.greater(d_, 0.2), x=ones, y=zeros)
+    d_2 = tf.where(tf.less(d_, -5.0), x=twos, y=zeros)
+    d_ = d_1 + d_2
+    # t_grad = tf.Print(d_, [d_], message="d_: ", first_n=10, summarize=10)
+    # d = tf.ones_like(s)
+    d_ = tf.cast(d_, tf.int32)
+    # d_ = d_ + 10 # TODO
+    # t_grad = tf.Print(d_, [d_], message="d_: ", first_n=10, summarize=10)
+    
+    d_ = tf.transpose(d_, [0, 2, 1])
+    print(d_.get_shape())
+    bins = tf.map_fn(bincount, d_) # shape=(128, 10, 12)
+    print(bins.get_shape())
+    bins = tf.cast(bins, tf.float32)
+    print(bins.dtype)
+    bins = bins + lap_noise_vote([FLAGS.batch_size, FLAGS.nb_labels, 3])
+    d = tf.argmax(bins, axis=-1) # shape=(128, 10)
+    # print(bins.get_shape())
+    # d = d - 10
+    ones = tf.ones_like(d)
+    tens = ones * (-10)
+    zeros = tf.zeros_like(d)
+    d1 = tf.where(tf.equal(d, 1), ones, zeros)
+    d2 = tf.where(tf.equal(d, 2), tens, zeros)
+    d = d1 + d2
+    d = tf.cast(d, tf.float32)
+    d = 0.5 * d
+    d = tf.expand_dims(d, 1)
+    s_grad = d * grad / FLAGS.batch_size
+
+    t_grad = tf.constant(np.zeros(10, dtype=np.float32))
+
+    t_grad = tf.tile(t_grad, [t.get_shape()[0]*t.get_shape()[1]])
+    t_grad = tf.reshape(t_grad, t.get_shape())
+
+    return s_grad, t_grad
+
+def batch_vote_gradient(op, grad):
+    s = op.inputs[0] # shape=(128, 1, 10)
+    t = op.inputs[1] # shape=(128, 100, 10)
+    # print(t.dtype)
+    t = t+1e-14
+    s = s+1e-14
+    denominator = tf.add(s, t, name='denominator')
+    tmp = 2.0*(s)/(denominator)
+    d_ = tf.log(tmp, name='derivation')
+    d_ = d_ / np.log(2.0)
+    # zeros = tf.zeros_like(d_)
+    # ones = tf.ones_like(d_)
+    # d_ = tf.where(tf.equal(t, zeros), ones, d_)
+    d_ = tf.clip_by_value(d_, clip_value_min=-10.0, clip_value_max=1.0)
+    
+    # compute every teacher's predict on mean value of batch's gradient
+    d_t = tf.transpose(d_, [2, 1, 0])
+    # print(d_t.get_shape())
+    d_batch_mean = tf.reduce_mean(d_t, axis=-1) # (10, 100)
+    # print(d_batch_mean.get_shape())
+    d_t = tf.round(d_batch_mean)
+    # print(d_t.get_shape())
+    d_t = d_t + 10 # (10, 100)
+    d_t = tf.cast(d_t, tf.int32)
+    # print(d_t.get_shape())
+    bins = tf.map_fn(bincount_, d_t) # (10, 12)
+    # print(bins.get_shape())
+    bins = tf.cast(bins, tf.float32)
+    bins = bins + lap_noise_vote([FLAGS.nb_labels, 12])
+    d_mean = tf.argmax(bins, axis=-1)
+    d_mean = d_mean - 10
+    d_mean = tf.cast(d_mean, tf.float32) # (10,)
+    d_true_mean = tf.reduce_mean(d_batch_mean, axis=-1) # (10,)
+    d_gap = d_mean - d_true_mean # (10,)
+    d_ = tf.reduce_mean(d_, axis=1) # (128, 10)
+    # print(d_.get_shape())
+    # d_ = tf.transpose(d_, [1, 0])
+    # print(d_.get_shape())
+    d_ = d_ + d_gap
+    # d_ = tf.transpose(d_, [1, 0])
+
+    d_ = tf.expand_dims(d_, axis=1)
+    s_grad = d_ * grad / FLAGS.batch_size
+
+
+    t_grad = tf.constant(np.zeros(10, dtype=np.float32))
+
+    t_grad = tf.tile(t_grad, [t.get_shape()[0]*t.get_shape()[1]])
+    t_grad = tf.reshape(t_grad, t.get_shape())
+
+    return s_grad, t_grad
+    
+def batch_vote_gradient_sparse(op, grad):
+    
+    s = op.inputs[0] # shape=(128, 1, 10)
+    t = op.inputs[1] # shape=(128, 100, 10)
+    # print(t.dtype)
+    t = t+1e-14
+    s = s+1e-14
+    denominator = tf.add(s, t, name='denominator')
+    tmp = 2.0*(s+1e-14)/(denominator) + 1e-14
+    d_ = tf.log(tmp, name='derivation')
+    d_ = d_ / np.log(2.0)
+    # zeros = tf.zeros_like(d_)
+    # ones = tf.ones_like(d_)
+    # d_ = tf.where(tf.equal(t, zeros), ones, d_)
+    d_ = tf.clip_by_value(d_, clip_value_min=-10.0, clip_value_max=1.0)
+    
+    # compute every teacher's predict on mean value of batch's gradient
+    d_t = tf.transpose(d_, [2, 1, 0])
+    # print(d_t.get_shape())
+    d_batch_mean = tf.reduce_mean(d_t, axis=-1) # (10, 100)
+    # print(d_batch_mean.get_shape())
+    d_t = tf.round(d_batch_mean)
+    # print(d_t.get_shape())
+    d_t = d_t + 10 # (10, 100)
+    d_t = tf.cast(d_t, tf.int32)
+    # print(d_t.get_shape())
+    bins = tf.map_fn(bincount_, d_t) # (10, 12)
+    # print(bins.get_shape())
+    bins = tf.cast(bins, tf.float32)
+    bins = bins + lap_noise_vote([FLAGS.nb_labels, 12])
+    d_mean = tf.argmax(bins, axis=-1)
+    d_mean = d_mean - 10
+    d_mean = tf.cast(d_mean, tf.float32) # (10,)
+    d_true_mean = tf.reduce_mean(d_batch_mean, axis=-1) # (10,)
+    d_gap = d_mean - d_true_mean # (10,)
+    d_ = tf.reduce_mean(d_, axis=1) # (128, 10)
+    # print(d_.get_shape())
+    # d_ = tf.transpose(d_, [1, 0])
+    # print(d_.get_shape())
+    d_ = d_ + d_gap
+    # d_ = tf.transpose(d_, [1, 0])
+
+    d_ = tf.expand_dims(d_, axis=1)
+    s_grad = d_ * grad / FLAGS.batch_size
+
 
     t_grad = tf.constant(np.zeros(10, dtype=np.float32))
 
